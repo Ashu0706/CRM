2bd860241288a3e184ec75fe5e0ca7f586df1311
Ashutosh Choudhary <ashu1303kr@gmail.com>
2025-03-06 08:56:32 +0530

    git commit -m "feat: Added login functionality and navigation bar"

--- website/views.py
+++ website/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth import authenticate , login , logout
+from django.contrib import messages
 
-# Create your views here.
+def home(request):
+    if request.method == 'POST':
+        #check to see if logged in
+        username = request.POST['username']
+        password = request.POST['password']
+        
+        #Authenticate the user
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request,user)
+            messages.success(request,"You have been logged in ")
+            return redirect('home')
+        else:
+            messages.success(request,"There is an error , please TRY again...")
+        
+    return render(request,'home.html',{})
+
+def login_user(request):
+    pass
+
+def logout_user(request):
+    pass
